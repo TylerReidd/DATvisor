933d1c91e2e1aed67bb1dfd637a28fe0f6b2e333
Tyler Reid <tylerreid1988@gmail.com>
2020-09-19 16:36:27 -0400

    add PostCard component

---
+++ src/components/PostCard/PostCard.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+
+function PostCard({ user, postArrival, handleDeletePostArrival}) {
+    return (
+        <>
+        <div className="card">
+            <h2>Post Arrival Itinerary</h2>
+            <ul>
+                <li>{postArrival.toDo}</li>
+                <li>{postArrival.activity}</li>
+                <li>{postArrival.time}</li>
+            </ul>
+        </div>
+        <>
+        <Link 
+        className="btn yellow black-text"
+        to={{
+            pathname: '/edit',
+            state: {postArrival}
+        }}>
+        Edit Activity
+        </Link>
+        </>
+        </>
+    )
+}
+
+export default PostCard;
