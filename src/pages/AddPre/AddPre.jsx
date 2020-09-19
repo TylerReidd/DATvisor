ce27e5b41a8874c6b0657d297b3863013567bdf9
David Anderson <djathree92@gmail.com>
2020-09-19 16:23:21 -0400

    add pre add page

---
+++ src/pages/AddPre/AddPre.jsx
@@ -0,0 +1,50 @@
+import React, { Component } from 'react'
+import './AddPre.css';
+
+class AddPre extends Component {
+    state = { 
+        formData: {
+            toDo: []
+        }
+    }
+
+    handleSubmit = e => {
+        e.preventDefault();
+        this.props.handleAddPre(this.state.formData)
+    }
+
+    handleChange = e => {
+        const formData = {...this.state.formData, [e.target.name]: e.target.value};
+        this.setState({
+        formData
+        });
+     }
+
+     formRef = React.createRef()
+
+    render() { 
+        return ( 
+            <>
+                <h1>Tasks to complete prior to Trip</h1>
+                <div className="AddPre" id="addPre">
+                    <form className="col s12" ref={this.formRef} onSubmit={this.handleSubmit}>
+                        <div className="row">
+                            <div className="input-field col s12">
+                            <label htmlFor="toDo">Task(s)</label>
+                            <input name="toDo" id="toDo" type="text" className="active" value={this.state.formData.toDo} onChange={this.handleChange} />
+                            </div>
+                        </div>
+                        <button
+                            type="submit"
+                            className="btn btn-danger"
+                                    >
+                            Add Pre-Arrival Itinerary
+                        </button>                           
+                    </form>
+                </div>
+            </>
+         );
+    }
+}
+ 
+export default AddPre;
