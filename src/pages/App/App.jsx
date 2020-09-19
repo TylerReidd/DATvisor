9484658a150f0fac8bd76235967b0978372430cc
David Anderson <djathree92@gmail.com>
2020-09-19 16:24:29 -0400

    import to app

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -7,11 +7,16 @@ import Landing from '../Landing/Landing'
 import Index from '../IndexPage/IndexPage'
 import authService from "../../services/authService";
 import Users from "../Users/Users";
+import PreArrivalList from '../PreArrivalList/PreArrivalList';
+import AddPre from '../AddPre/AddPre'
+import * as preArrivalAPI from '../../services/pre-api'
 import "./App.css";
 
 
 class App extends Component {
   state = {
+    preArrivals: [],
+    postArrivals: [],
     user: authService.getUser(),
   };
 
@@ -24,6 +29,13 @@ class App extends Component {
     this.setState({ user: authService.getUser() });
   };
 
+  handleAddPre = async newPreData => {
+    const newPreArrival = await preArrivalAPI.create(newPreData);
+    this.setState(state => ({
+      preArrivals: [...state.preArrivals, newPreArrival]
+    }), () => this.props.history.push('/preArrival')) 
+  }
+
   render() {
     const {user} = this.state
     return (
@@ -35,7 +47,7 @@ class App extends Component {
           render={({history}) => (
             <main>
               <Landing />
-            <Login
+              <Login
               history={history}
               handleSignupOrLogin={this.handleSignupOrLogin}
             />
@@ -69,10 +81,28 @@ class App extends Component {
           render={() => (user ? <Users /> : <Redirect to="/login" />)}
         />
         <Route 
-        exact
-        path="/index"
-        render={() => 
-        <Index /> } />
+          exact
+          path="/index"
+          render={() => 
+            <Index /> } />
+        <Route 
+          exact
+          path="/preArrival"
+          render={() =>
+            <PreArrivalList 
+              preArrivals={this.state.preArrivals}
+        /> } />
+        <Route
+          exact
+          path="/addPreArrival"
+          render={() =>
+            authService.getUser() ?
+              <AddPre
+                handleAddPre={this.handleAddPre}
+                />
+                :
+                <Redirect to='/login' />
+          } />
       </>
     );
   }
