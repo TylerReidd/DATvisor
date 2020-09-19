f299d51efa5d2325cbd0872185e0742b68ce129f
David Anderson <djathree92@gmail.com>
2020-09-19 16:27:48 -0400

    add pre index page

---
+++ src/pages/PreArrivalList/PreArrivalList.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PreCard from '../../components/PreCard/PreCard';
+import './PreArrivalList.css';
+
+const PreArrivalList = (props) => {
+    return (
+        <>
+            <h1>Pre-Arrival List</h1>
+            <div className='PreArrivalList-grid'>
+                {props.preArrivals.map(preArrival => 
+                <PreCard
+                    key={preArrival._id}
+                    preArrival={preArrival}
+                    />
+                    )}
+            </div>
+            <Link
+            to={{
+                pathname: '/addPreArrival',
+                state: {}
+            }}>
+
+                <button type="button" class="btn btn-success">Add Pre-Arrival Itinerary</button>
+            </Link>
+        </>
+    )
+}
+ 
+export default PreArrivalList;
