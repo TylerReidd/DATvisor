aac1989bd4efff172c0fcd8b871485f8ffcc0bde
Tyler Reid <tylerreid1988@gmail.com>
2020-09-21 21:35:10 -0400

    add post arrival stuff

--- src/pages/PostArrivalList/PostArrivalPage.jsx
+++ src/pages/PostArrivalList/PostArrivalPage.jsx
@@ -3,10 +3,12 @@ import { Link } from 'react-router-dom'
 import PostCard from '../../components/PostCard/PostCard'
 import './PostArrivalPage.css'
 
+
 const PostList = (props) => {
     return(
         <> 
         <h1>Post-Arrival Itinerary</h1>
+        <div className='container'>
         <div>
             {props.postArrivals.map(postArrival =>
                 <PostCard
@@ -20,12 +22,52 @@ const PostList = (props) => {
         <Link
         to={{
             pathname: '/postToDo',
-            state: {}
+            state: {
+                postArrivals: []
+            }
         }}>
-        <button type="button" class="btn btn-success">ADD Activity</button>
+            <br/>
+            <br/>
+
+        <button type="button" className="btn btn-success btn-med">ADD Activity</button>
         </Link>
+        </div>
+
+            
         </>
     )
 }
+    
+
+
+//     return(
+//         <> 
+//         <h1>Post-Arrival Itinerary</h1>
+//         <div className='container'>
+//         <div>
+//             {props.postArrivals.map(postArrival =>
+//                 <PostCard
+//                 key={postArrival._id}
+//                 postArrival={props.postArrivals}
+//                 handleDeletePostArrival={props.handleDeletePostArrival}
+//                 user={props.user}
+//                 />
+//                 )}
+//         </div>
+//         <Link
+//         to={{
+//             pathname: '/postToDo',
+//             state: {
+//                 postArrivals: []
+//             }
+//         }}>
+//         <button type="button" className="btn btn-success">ADD Activity</button>
+//         </Link>
+//         </div>
+
+            
+//         </>
+//     )
+// }
 
 export default PostList
