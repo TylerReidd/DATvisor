fb2f6b7171fbb7c9fedff94e836370534f5e95bd
AndersonAbreuJr <68236072+AndersonAbreuJr@users.noreply.github.com>
2020-09-22 12:41:48 -0400

    add api onto indexpage

--- src/pages/IndexPage/IndexPage.jsx
+++ src/pages/IndexPage/IndexPage.jsx
@@ -1,11 +1,16 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './IndexPage.css'
 import { Link } from 'react-router-dom'
+import { getLocationID } from '../../services/locationID';
 
-function Index () {
-    return (
-        <>
-        <h1>Index Baby</h1>
+const IndexPage = () => {
+    const [searchValue, setSearchValue] = useState('')
+        return (
+            <>
+            <h1>DATvisor</h1>
+            <b> Where are you going? </b><br /><br />
+            <input type="text" id="location" name="location" value={searchValue} onChange={event => setSearchValue(event.currentTarget.value)}/><br /><br />
+            <button className="btn btn-primary" type="submit" onClick={()=> getLocationID({searchValue}) }> ENTER </button>
             <Link 
             to="/preArrival"
             >
@@ -15,9 +20,8 @@ function Index () {
             to='/post'>
                 Button
             </Link>
-         </> 
-        
-    )
+            </>
+         );
 }
-
-export default Index
+ 
+export default IndexPage;
