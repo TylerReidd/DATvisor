1de580393c3a71db15ebc2838be89486a14e9956
David Anderson <djathree92@gmail.com>
2020-09-21 21:15:55 -0400

    add link to pre page on index

--- src/pages/IndexPage/IndexPage.jsx
+++ src/pages/IndexPage/IndexPage.jsx
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import './IndexPage.css'
+import { Link } from 'react-router-dom'
 
 function Index () {
     return (
         <>
         <h1>Index Baby</h1>
+            <Link 
+            to="/preArrival"
+            >
+                <button>Pre</button>
+            </Link>
          </> 
+        
     )
 }
 
