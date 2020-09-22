75257669bfc735c5717efe6423e4dbd4b38703c8
David Anderson <djathree92@gmail.com>
2020-09-21 21:18:18 -0400

    add auth to getall

--- src/services/pre-api.js
+++ src/services/pre-api.js
@@ -11,8 +11,11 @@ export function create(preArrival) {
 }
 
 export function getAll() {
-  return fetch(BASE_URL, {mode: "cors"})
-  .then(res => res.json())
+  return fetch(BASE_URL, {
+      method: "GET",
+      headers: { 'content-type': 'application/json', 'Authorization': 'Bearer ' + tokenService.getToken() },
+  }, { mode: "cors" })
+  .then(res => res.json());
 }
 
 export function deleteOne(id) {
