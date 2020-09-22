4ff1250a98c5b1dce147be07414e760fbd78a3c9
David Anderson <djathree92@gmail.com>
2020-09-22 16:41:14 -0400

    add to preschema

--- models/preArrival.js
+++ models/preArrival.js
@@ -5,7 +5,8 @@ const Schema = mongoose.Schema;
 const preSchema = new Schema ({
     toDo: {type: []},
     time: Date,
-    done: Boolean
+    done: Boolean,
+    myTrip: {type: Schema.Types.ObjectId, ref: 'MyTrip'}
 }, {timestamps: true})
 
 module.exports = mongoose.model('PreArrival', preSchema)
