3831a3b0ad29358cce9d6e9fc071b85900dbf7ec
David Anderson <djathree92@gmail.com>
2020-09-23 11:05:15 -0400

    test exclusive

--- src/components/PreCard/PreCard.jsx
+++ src/components/PreCard/PreCard.jsx
@@ -1,30 +1,64 @@
+// import React from 'react';
+// import { Link } from 'react-router-dom';
+
+// function PreCard({ user, preArrival, handleDeletePreArrival}) {
+//     return (
+//         <>
+//         <div>
+//             <div>
+//                 <li>{preArrival.toDo}</li>
+//             </div>
+//         </div>
+//         <>
+//         <Link 
+//         className="btn yellow black-text"
+//         to={{
+//             pathname: '/edit',
+//             state: {preArrival}
+//         }}>
+//         <button type="submit" className="btn btn-warning btn-sm">Edit</button>
+//         </Link>
+//         </>
+//         <>
+//         <button type="submit" className="btn btn-danger btn-sm" onClick={() => handleDeletePreArrival(preArrival._id)}>
+//             Delete 
+//         </button>
+//         </>
+//         </>
+//     )
+// }
+// export default PreCard;
+
 import React from 'react';
 import { Link } from 'react-router-dom';
 
 function PreCard({ user, preArrival, handleDeletePreArrival}) {
     return (
         <>
-        <div>
-            <div>
-                <li>{preArrival.toDo}</li>
-            </div>
-        </div>
-        <>
-        <Link 
-        className="btn yellow black-text"
-        to={{
-            pathname: '/edit',
-            state: {preArrival}
-        }}>
-        <button type="submit" className="btn btn-warning btn-sm">Edit</button>
-        </Link>
-        </>
-        <>
-        <button type="submit" className="btn btn-danger btn-sm" onClick={() => handleDeletePreArrival(preArrival._id)}>
-            Delete 
-        </button>
-        </>
+        {user._id ? 
+            <>
+                <div>
+                    <div>
+                        <li>{preArrival.toDo}</li>
+                    </div>
+                </div>
+                    <Link 
+                    className="btn yellow black-text"
+                    to={{
+                        pathname: '/edit',
+                        state: {preArrival}
+                    }}>
+                    <button type="submit" className="btn btn-warning btn-sm">Edit</button>
+                    </Link>
+                        <button type="submit" className="btn btn-danger btn-sm" onClick={() => handleDeletePreArrival(preArrival._id)}>
+                            Delete 
+                        </button>
+            </>
+            :
+            ''
+        }
         </>
     )
 }
+
 export default PreCard;
