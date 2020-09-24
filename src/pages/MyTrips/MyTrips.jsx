4984f838774b492e4f64313966979e1f621404a1
David Anderson <djathree92@gmail.com>
2020-09-24 14:34:22 -0400

    make add trip

--- src/pages/MyTrips/MyTrips.jsx
+++ src/pages/MyTrips/MyTrips.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import './MyTrips.css'
+import { Link } from 'react-router-dom'
 
 const MyTripsPage = (props) => {
     return ( 
+        <>
         <h1>WORK</h1>
+        
+        <Link to=
+        {{pathname: "/addTrip"
+            }}> <button>Add Trip</button> </Link>
+        </>
      );
 }
  
