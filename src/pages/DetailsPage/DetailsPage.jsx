1223e88635309cb7f892f02524c0abd9c794e92e
Tyler Reid <tylerreid1988@gmail.com>
2020-09-24 23:42:59 -0400

    commit

---
+++ src/pages/DetailsPage/DetailsPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './DetailsPage.css'
+
+const DetailsPage = () => {
+    return ( 
+        <>
+        <h1>Details Page</h1>
+        </>
+     );
+}
+ 
+export default DetailsPage;
